dfa9afdbb1731f4fc5c681f3036087e05c6bae7e
Efrana Jannat <efrana.jannat@gmail.com>
2020-05-12 00:06:17 +0600

    second commit

--- Employer/views.py
+++ Employer/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
-# from django.http import HttpResponse
-# def index(request):
-#     return HttpResponse(" Django on Docker  ")
+Create your views here.
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse(" Django on Docker ")
